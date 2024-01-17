29868f5242a192da8649f145e07a4dcd9c3524a3
Rodrigo Viannini <137203101+engenheirorodrigoviannini@users.noreply.github.com>
2024-01-17 13:04:34 -0300

    Update 03_gemini_basic_chat_voz.py

--- 03_gemini_basic_chat_voz.py
+++ 03_gemini_basic_chat_voz.py
@@ -9,69 +9,69 @@ def main():
         config_data = json.load(config_file)
         google_api_key = config_data.get('google_api_key')
 
-    genai.configure(api_key=google_api_key)
-    for m in genai.list_models():
-        if 'generateContent' in m.supported_generation_methods:
-            print(m.name)
+        genai.configure(api_key=google_api_key)
+        for m in genai.list_models():
+            if 'generateContent' in m.supported_generation_methods:
+                print(m.name)
 
-    model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat(history=[])
+        model = genai.GenerativeModel('gemini-pro')
+        chat = model.start_chat(history=[])
 
-    ### configura voz
-    if assistente_falante:
-        import pyttsx3
-        engine = pyttsx3.init()
-
-        voices = engine.getProperty('voices')
-        engine.setProperty('rate', 180) # velocidade 120 = lento
-
-        print("\nLista de Vozes - Verifique o número\n")
-        for indice, vozes in enumerate(voices): # listar vozes
-            print(indice, vozes.name)
-
-        voz = 1
-        engine.setProperty('voice', voices[voz].id)
-
-    if ligar_microfone:
-        import speech_recognition as sr  # pip install SpeechRecognition
-        r = sr.Recognizer()
-        mic = sr.Microphone()
-
-    bem_vindo = "# Bem Vindo ao Assistente com Gemini AI #"
-    print("")
-    print(len(bem_vindo) * "#")
-    print(bem_vindo)
-    print(len(bem_vindo) * "#")
-    print("###   Digite 'desligar' para encerrar    ###")
-    print("")
-
-    while True:
-        if ligar_microfone:
-            with mic as fonte:
-                r.adjust_for_ambient_noise(fonte)
-                print("Fale alguma coisa (ou diga 'desligar')")
-                audio = r.listen(fonte)
-                print("Enviando para reconhecimento")
-                try:
-                    texto = r.recognize_google(audio, language="pt-BR")
-                    print("Você disse: {}".format(texto))
-                except Exception as e:
-                    print("Não entendi o que você disse. Erro", e)
-                    texto = ""
-        else:
-            texto = input("Escreva sua mensagem (ou #sair): ")
-
-        if texto.lower() == "desligar":
-            break
-
-        response = chat.send_message(texto)
-        print("Gemini:", response.text, "\n")
-
+        ### configura voz
         if assistente_falante:
-            engine.say(response.text)
-            engine.runAndWait()
+            import pyttsx3
+            engine = pyttsx3.init()
 
-    print("Encerrando Chat")
+            voices = engine.getProperty('voices')
+            engine.setProperty('rate', 180) # velocidade 120 = lento
+
+            print("\nLista de Vozes - Verifique o número\n")
+            for indice, vozes in enumerate(voices): # listar vozes
+                print(indice, vozes.name)
+
+            voz = 1
+            engine.setProperty('voice', voices[voz].id)
+
+        if ligar_microfone:
+            import speech_recognition as sr  # pip install SpeechRecognition
+            r = sr.Recognizer()
+            mic = sr.Microphone()
+
+        bem_vindo = "# Bem Vindo ao Assistente com Gemini AI #"
+        print("")
+        print(len(bem_vindo) * "#")
+        print(bem_vindo)
+        print(len(bem_vindo) * "#")
+        print("###   Digite 'desligar' para encerrar    ###")
+        print("")
+
+        while True:
+            if ligar_microfone:
+                with mic as fonte:
+                    r.adjust_for_ambient_noise(fonte)
+                    print("Fale alguma coisa (ou diga 'desligar')")
+                    audio = r.listen(fonte)
+                    print("Enviando para reconhecimento")
+                    try:
+                        texto = r.recognize_google(audio, language="pt-BR")
+                        print("Você disse: {}".format(texto))
+                    except Exception as e:
+                        print("Não entendi o que você disse. Erro", e)
+                        texto = ""
+            else:
+                texto = input("Escreva sua mensagem (ou #sair): ")
+
+            if texto.lower() == "desligar":
+                break
+
+            response = chat.send_message(texto)
+            print("Gemini:", response.text, "\n")
+
+            if assistente_falante:
+                engine.say(response.text)
+                engine.runAndWait()
+
+        print("Encerrando Chat")
 
 if __name__ == '__main__':
     main()
